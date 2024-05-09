031a1aa38d740e4e89a18487c4b9189a1c7867cb
nonKanisorn <144541997+nonkanisorn@users.noreply.github.com>
2024-05-09 14:23:04 +0700

    Update Reportcase.jsx
    addFunc:เพิ่มข้อมูลช่างลงDB tbl_case

--- client/src/components/Manager/Reportcase.jsx
+++ client/src/components/Manager/Reportcase.jsx
@@ -1,21 +1,47 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Button from "@mui/material/Button";
 
 
 function Reportcase() {
+  const [nametech, setNametech] = useState([]);
+  const [selectedTechnicians, setSelectedTechnicians] = useState({});
   const [caseData, setcaseData] = useState([]);
-
-  useEffect(()=>{
+  const handleChange = (event, case_id) => {
+    const selectedTechnician = event.target.value; 
+    setSelectedTechnicians((prevState) => ({
+      ...prevState,
+      [case_id]: selectedTechnician,
+    }));
+  };
+  const sendtech = (case_id) => {
+    const technician_name = selectedTechnicians[case_id];
     axios
-      .get("http://localhost:5000/Case")
+      .patch(`http://localhost:5000/addtechcase/${case_id}`, {
+        technician_name,
+      })
+      .then(() => {
+        console.log("success");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/case/")
       .then(function (response) {
         setcaseData(response.data);
         console.log(response);
@@ -24,8 +50,18 @@ function Reportcase() {
         console.log(error);
       })
       .finally(function () {});
-  },[])
-
+    axios
+      .get("http://localhost:5000/technician")
+      .then(function (response) {
+        const technicianData = response.data.map(
+          (technician) => technician.technician_name
+        );
+        setNametech(technicianData);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <TableContainer component={Paper}>
@@ -45,7 +81,7 @@ function Reportcase() {
           {caseData.map((item, index) => (
             <TableRow
               key={index}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row">
                 {item.case_id}
@@ -54,9 +90,33 @@ function Reportcase() {
               <TableCell>{item.case_detail}</TableCell>
               <TableCell>{item.case_detail}</TableCell>
               <TableCell>{item.case_detail}</TableCell>
-              <TableCell><Button size="small"sx={{fontSize: 8, padding: 0.5}}variant="contained">Assign</Button></TableCell>
-
-              
+              <TableCell>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">ช่าง</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={selectedTechnicians[item.case_id] || item.technician_name}
+                    label="ช่าง"
+                    onChange={(event) => handleChange(event, item.case_id)}
+                  >
+                    {Array.isArray(nametech) &&
+                      nametech.map((name, index) => (
+                        <MenuItem key={index} value={name}>
+                          {name}
+                        </MenuItem>
+                      ))}
+                  </Select>
+                </FormControl>
+              </TableCell>
+              <TableCell>
+                <Button
+                  onClick={() => sendtech(item.case_id)}
+                  variant="contained"
+                >
+                  ดูรายละเอียด 
+                </Button>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
@@ -64,4 +124,5 @@ function Reportcase() {
     </TableContainer>
   );
 }
+
 export default Reportcase;
