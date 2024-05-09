5b154b4957b070b12c1369ce43da8fa16784f089
nonKanisorn <144541997+nonkanisorn@users.noreply.github.com>
2024-05-09 14:23:51 +0700

    Update Managedepartment.js

--- server/Controllers/Managedepartment.js
+++ server/Controllers/Managedepartment.js
@@ -1,9 +1,10 @@
 const mysql = require("mysql");
+require('dotenv').config();
 const db = mysql.createConnection({
-  user: "root",
-  host: "localhost",
-  password: "",
-  database: "repairnoticedb",
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 exports.list = async (req, res) => {
