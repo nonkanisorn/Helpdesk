3f8f0a3e7a78ff97b4d79f5b04f3b69c21fbf2dc
nonKanisorn <144541997+nonkanisorn@users.noreply.github.com>
2024-05-09 14:23:59 +0700

    Update Managestatus.js

--- server/Controllers/Managestatus.js
+++ server/Controllers/Managestatus.js
@@ -1,10 +1,13 @@
 const mysql = require("mysql");
+require('dotenv').config();
 const db = mysql.createConnection({
-  user: "root",
-  host: "localhost",
-  password: "",
-  database: "repairnoticedb",
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
+
+
 
 exports.list = async (req, res) => {
   db.query("SELECT * FROM tbl_status ", (err, results) => {
