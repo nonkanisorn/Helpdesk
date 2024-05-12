4ff44683e829b424072a3da3065c515590d71b0a
nonKanisorn <144541997+nonkanisorn@users.noreply.github.com>
2024-05-13 01:36:39 +0700

    Update ManageCase.js

--- server/Routes/ManageCase.js
+++ server/Routes/ManageCase.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const {list, create,remove, update} = require('../Controllers/ManageCase')
+const {list, create,remove, update, listbyID} = require('../Controllers/ManageCase')
 
 router.get('/Case',list)
+router.get('/Case/:case_id',listbyID)
 router.post('/Case',create)
 router.delete('/Case/:case_id',remove)
 router.put('/Case/:case_id',update)
