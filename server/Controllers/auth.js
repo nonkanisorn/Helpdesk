a30ab04e4878a1fb97aec72f4b54ba8634ec387d
nonKanisorn <144541997+nonkanisorn@users.noreply.github.com>
2024-06-04 20:19:57 +0700

    Create auth.js

---
+++ server/Controllers/auth.js
@@ -0,0 +1,78 @@
+const mysql = require("mysql")
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const db = mysql.createConnection({
+
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+})
+
+
+exports.register = async (req, res) => {
+  try {
+    const { username, userpassword } = req.body
+    const passwordHash = await bcrypt.hash(userpassword, 10)  //Encrypt
+    db.query("SELECT * FROM tbl_users WHERE username = ? ", [username], (error, results) => {
+      if (error) {
+        res.send("server error").status(500)
+      }
+      if (results.length > 0) {
+        res.send('มีคนใช้แล้ว').status(400)
+      }
+      db.query("INSERT INTO tbl_users (username,userpassword)values(?,?)", [username, passwordHash]), (err, results) => {
+        if (err) {
+          console.log(err)
+          res.status(501).send("server error")
+        }
+      }
+      res.send('ลงทะเบียนสำเร็จ').status(200)
+    })
+    //check user user ว่าซ้ำไหม
+    //encrypt ท่าusernameไม่ซ้ำ จะเข้ารหัสuserpassword
+    //save saveข้อมูลลงdb
+  } catch (err) {
+    res.send('Server error').status(500)
+  }
+}
+
+exports.login = async (req, res) => {
+  try {
+    //check user 1.เช็คusername ว่าในdatabaseไหม ท่าไม่มี 401 2 ท่ามีไปเช็ตpassword ต่อ 3 จากนั้น สร้างpayload ที่จะส่งไปหน้าบ้าน 4.สร้างtoken
+    const { username, userpassword } = req.body
+    db.query("SELECT * FROM tbl_users WHERE username = ? ", [username], async (error, results) => {
+
+      if (error) {
+        res.send(err).status(500)
+      }
+
+      if (results.length > 0) {
+
+        const isMatch = await bcrypt.compare(userpassword, results[0].userpassword)
+        if (!isMatch) {
+          return res.status(400).send('passผิด')
+        }
+        console.log(isMatch)
+        var payload = {
+          user: {
+            name: results[0].username
+          }
+        }
+      } else {
+        return res.status(400).send("user not found")
+      }
+
+      jwt.sign(payload, 'jwtsecret', { expiresIn: '1d' }, (err, token) => {
+        if (err) throw err;
+        res.json({ token, payload })
+      })
+
+
+    })
+  } catch (err) {
+    console.log((err))
+    res.send('server error').status(500)
+  }
+}
