6c7f1b9e262b03c895b952140f702ec0a287b816
nonKanisorn <144541997+nonkanisorn@users.noreply.github.com>
2024-07-19 03:21:43 +0700

    Update auth.js

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const { register, login, currentUser } = require('../Controllers/auth')
-const { auth } = require('../Middleware/auth')
-
+const { adminCheck, auth } = require('../Middleware/auth')
 router.post('/register', register)
 router.post('/login', login)
 router.post('/current-user', auth, currentUser)
+router.post('/current-admin', auth, adminCheck, currentUser)
+// router.post('/auth', auth)
 module.exports = router
