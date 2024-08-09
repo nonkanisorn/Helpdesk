0a13318e9b07a0589fbc27ae228c79ca4c3052be
nonKanisorn <144541997+nonkanisorn@users.noreply.github.com>
2024-08-10 02:55:41 +0700

    Route

---
+++ client/src/Routes/ManagerRoute.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Sidebarmanager from '../layout/manager/Sidebarmanager';
+import Headerbarmanager from '../layout/manager/Headerbarmanager';
+import { Box } from '@mui/material';
+import Reportcase from '../components/Manager/Reportcase';
+import Detailcase from '../components/Manager/Detailcase';
+import Adduser from '../components/Manager/Adduser';
+import { useSelector } from 'react-redux';
+import Managerpages from '../components/Manager/Managerpages';
+import { useEffect } from 'react';
+import Notfound404 from '../components/Notfound404';
+const ManagerRoute = () => {
+  const navigate = useNavigate()
+  const { user } = useSelector((state) => ({ ...state }))
+  console.log('manager', user)
+  useEffect(() => {
+    if (!user || !user.token) {
+      navigate('/login')
+    }
+  }, [user, navigate])
+  const text = "No permission"
+  return user && user.token && user.role === 2 ? (
+    <div className="app">
+      <Sidebarmanager />
+      <main className="content">
+        <Headerbarmanager />
+        <div className="content_body">
+          <Box m="20px">
+            <Routes>
+              <Route path="index" element={<Managerpages />} />
+              <Route path="reportcase" element={<Reportcase />} />
+              <Route path="detail/:case_id" element={<Detailcase />} />
+              <Route path="adduser" element={<Adduser />} />
+              <Route path="*" element={<Notfound404 />} />
+            </Routes>
+          </Box>
+        </div>
+      </main>
+    </div>
+  ) : <Notfound404 text={text} />;
+};
+
+export default ManagerRoute;
---
+++ client/src/Routes/TechnicianRoute.jsx
@@ -0,0 +1,45 @@
+
+import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Sidebarmanager from '../layout/manager/Sidebarmanager';
+import Headerbarmanager from '../layout/manager/Headerbarmanager';
+import { Box } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import Notfound404 from '../components/Notfound404';
+import TechnicianPage from '../components/technician/TechnicianPage';
+import Reportcasetech from '../components/technician/Reportcasetech';
+import Sidebartech from '../layout/tech/Sidebartech';
+import Headerbartech from '../layout/tech/Headerbartech';
+import Detailcasetech from '../components/technician/Detailcasetech';
+const TechnicianRoute = () => {
+  const navigate = useNavigate()
+  const { user } = useSelector((state) => ({ ...state }))
+  console.log('tech', user)
+  useEffect(() => {
+    if (!user || !user.token) {
+      navigate('/login')
+    }
+  }, [user, navigate])
+  const text = "No permission"
+  return user && user.token && user.role === 3 ? (
+    <div className="app">
+      <Sidebartech />
+      <main className="content">
+        <Headerbartech />
+        <div className="content_body">
+          <Box m="20px">
+            <Routes>
+              <Route path="index" element={<TechnicianPage />} />
+              <Route path="*" element={<Notfound404 />} />
+              <Route path='reportcasetech' element={<Reportcasetech />} />
+              <Route path='Detailcasetech/:case_id' element={<Detailcasetech />} />
+            </Routes>
+          </Box>
+        </div>
+      </main>
+    </div>
+  ) : <Notfound404 text={text} />;
+};
+
+export default TechnicianRoute;
